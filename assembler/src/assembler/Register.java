f6dc848d2a6b21dfcb27b7c31b6177e8bbbfcaec
Cristian Ruiz <cristianruiz@Cristians-MacBook-Pro-2.local>
2019-10-04 01:15:22 -0400

    adding project structure classes

---
+++ assembler/src/assembler/Register.java
@@ -0,0 +1,16 @@
+package assembler;
+
+public class Register {
+
+	public final String r0 = "00";
+	public String r1 = "";
+	public String r2 = "";
+	public String r3 = "";
+	public String r4 = "";
+	public String r5 = "";
+	public String r6 = "";
+	public String r7 = "";
+	
+	public String sp = "";
+	public String pc = "";
+}
