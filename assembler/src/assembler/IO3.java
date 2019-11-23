5f100c8988292d8dc3428b567fe568ba196861a0
Hekam <Hekam@MSI>
2019-11-22 21:23:14 -0400

    IO3 refactor and implemented Launch method to launch from GUI(Still not fully functioning)

--- assembler/src/assembler/IO3.java
+++ assembler/src/assembler/IO3.java
@@ -6,17 +6,22 @@ import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import assembler.IO2_SevenSegmentDisplay.SevenSegments;
+
 public class IO3 {
 	
 	public static char[] result = new char [8];
-	public static ArrayList<String> ref = new ArrayList<>();
 	private static Object[][] IO3 = new Object[9][2];
 	private static String[] columnNames = { "Direction", "Content" };
-	private static int DirRows = 0;
-	private static int DirectionReference = 40;
+	private static JFrame frame = new JFrame();
+	private static ASCII convert = new ASCII();
+    private static ArrayList<String> ref = new ArrayList<>();
+
 	    
-	public static void main(String[] args) {
-	 	ref.add("01000011");
+	public static void main(String[] args) throws InterruptedException {
+	
+      
+		ref.add("01000011");
 		ref.add("01011111");
 		ref.add("01110110");
 		ref.add("01000100");
@@ -24,13 +29,14 @@ public class IO3 {
 		ref.add("01100011");
 		ref.add("01110111");
 		ref.add("01100000");
-      
-		
 		JFrame frame = initializeFrame();
-		JTable table1 = TableCreation();
-		frame.add(table1);
+		ASCII convert = new ASCII();
+		frame.add(ASCII.table1);
 	
-
+       convert.FillTable(ref);
+	}
+	public ASCII getAscii() {
+		return convert;
 	}
 	
 	
@@ -43,33 +49,56 @@ public class IO3 {
 		return frame;
 	}
 	
-	private static JTable TableCreation() {
-		
-        JTable table1 =new JTable(IO3, columnNames);
-        DefaultTableModel model = new DefaultTableModel(IO3, columnNames);
-        table1.setModel(model);
-		table1.setBackground(Color.gray);
-		table1.setBounds(5,20, 200, 150);
-		table1.setFont(new Font("Tahome",Font.ITALIC,14));
-		table1.setGridColor(Color.BLUE);
-		
-		for(int i = 0; i < 8 ; i++) {
-			model.setValueAt(ref.get(i), DirRows, 1);
-			 DirRows++;
+     static class ASCII extends JTable {
+    	 
+ 		private static final long serialVersionUID = 1L;
+        private static JTable table1 = new JTable(IO3, columnNames);
+        private static DefaultTableModel model;
+        private static int DirRows = 0;
+    	private static int DirectionReference = 40;
+  
+        
+        private static void main(String[] args) {
+        	
+           }
+        
+        public ASCII() {
+        	this.model = new DefaultTableModel(IO3, columnNames);
+        	table1.setModel(model);
+        	this.table1.setBackground(Color.gray);
+        	this.table1.setBounds(5,20, 200, 150);
+      		this.table1.setFont(new Font("Tahome",Font.ITALIC,14));
+      		this.table1.setGridColor(Color.BLUE);
+        }
+    
+        public static void FillTable(ArrayList<String> data) throws InterruptedException {
+        	
+    		for(int i = 0; i < 8 ; i++) {
+    			table1.setValueAt(ref.get(i), DirRows, 1);
+    			 DirRows++;
+    		}
+    		DirRows = 0;
+    		for(int i = 0; i < 8 ; i++) {
+    			table1.setValueAt(DirectionReference, DirRows, 0);
+    			 DirRows++;
+    			 DirectionReference++;
+    		}
+    		table1.setValueAt("Ascii result:", 8, 0);
+    		table1.setValueAt(AsciConversion(ref,result), 8, 1);
+        }
+        
+        
+        public void launch() {
+			frame.add(table1);
+			frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+			frame.setSize(250,300);
+			frame.setResizable(false);
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
 		}
-		DirRows = 0;
-		for(int i = 0; i < 8 ; i++) {
-			model.setValueAt(DirectionReference, DirRows, 0);
-			 DirRows++;
-			 DirectionReference++;
-		}
-		model.setValueAt("Ascii result:", 8, 0);
-		model.setValueAt(AsciConversion(ref,result), 8, 1);
-		return table1;
+		
+     }
 
-	}
-		
-	
 	public static String AsciConversion(ArrayList<String> data , char[] data2) {
 		int bintoDec;
 		String bin;
